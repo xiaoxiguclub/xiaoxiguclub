b56efed67c539a37181c724b43293544c300b74f
lianggepeinixuejava <lianggepeinixuejava@gmail.com>
2018-12-30 01:19:51 +0800

    添加360网站安全检测代码验证

--- src/main/java/com/lianggege/xiaoxiguclub/XiaoxiguclubApplication.java
+++ src/main/java/com/lianggege/xiaoxiguclub/XiaoxiguclubApplication.java
@@ -3,10 +3,17 @@ package com.lianggege.xiaoxiguclub;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.lianggege.xiaoxiguclub.mapper")
-public class XiaoxiguclubApplication {
+public class XiaoxiguclubApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(XiaoxiguclubApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(XiaoxiguclubApplication.class, args);
